2e8e22e82312be03e38d54f1a93436f35132df61
sndevopsnerd <jyotsna.jaisankar@glidefast.com>
2025-04-27 00:40:32 +0000

    OPS-7: Implement deposit and withdrawal limits

--- bank.py
+++ bank.py
@@ -4,13 +4,16 @@ class BankAccount:
 
     def deposit(self, amount):
         if amount <= 50:
-            raise ValueError("Deposit at least 50.")
+            raise ValueError("Please git add .
+git commit -m "OPS-7: Implement deposit and withdrawal limits"
+git push origin OPS-7-Add-Withdraw-and-Deposit-Limits
+Deposit at least 50.")
         self.balance += amount
         return self.balance
 
     def withdraw(self, amount):
         if amount <= 50 or amount > self.balance:
-            raise ValueError("Invalid withdrawal amount. Withdraw a minimum of USD 50")
+            raise ValueError("Invalid withdrawal amount. Please Withdraw a minimum of USD 50")
         self.balance -= amount
         return self.balance
 
