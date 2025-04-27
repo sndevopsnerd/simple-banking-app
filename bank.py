afedcf301c1e7063a71c92b10aeffe6e8061deb2
sndevopsnerd <jyotsna.jaisankar@glidefast.com>
2025-04-27 20:28:47 +0000

    OPS-8 Update Jenkinsfile with Pipelingit add .e

--- bank.py
+++ bank.py
@@ -4,13 +4,13 @@ class BankAccount:
 
     def deposit(self, amount):
         if amount <= 50:
-            raise ValueError("Please Deposit at least USD 50!!!")
+            raise ValueError("Please Deposit at least USD 50!!!!")
         self.balance += amount
         return self.balance
 
     def withdraw(self, amount):
         if amount <= 50 or amount > self.balance:
-            raise ValueError("Invalid withdrawal amount. Please Withdraw a minimum of USD 50")
+            raise ValueError("Invalid withdrawal amount! Please Withdraw a minimum of USD 50")
         self.balance -= amount
         return self.balance
 
