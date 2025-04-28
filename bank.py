30b3f595f37fa3fa8a37ae33d220b2b3cff5054a
sndevopsnerd <jyotsna.jaisankar@glidefast.com>
2025-04-28 03:20:53 +0000

    OPS-8 Updated URL and typo

--- bank.py
+++ bank.py
@@ -10,7 +10,7 @@ class BankAccount:
 
     def withdraw(self, amount):
         if amount <= 50 or amount > self.balance:
-            raise ValueError("Invalid withdrawal amount! Please Withdraw a minimum of USD 50!")
+            raise ValueError("Invalid withdrawal amount! Please Withdraw a minimum of USD 50!!")
         self.balance -= amount
         return self.balance
 
